ff7d475c50840c4e2e886b3e690857afda4df18d
pjstrauss12 <parkerstrauss21@gmail.com>
2023-02-28 13:12:22 -0500

    Initial commit

---
+++ src/card-list.js
@@ -0,0 +1,90 @@
+import { LitElement, html, css } from 'lit';
+import "@lrnwebcomponents/meme-maker/meme-maker.js";
+import "@fmk5108/building-card/src/building-card.js";
+import "coaster-card/src/coaster-card.js";
+
+const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
+
+
+class CardList extends LitElement {
+  static properties = {
+    header: { type: String },
+  }
+
+  static styles = css`
+    :host {
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: flex-start;
+      font-size: calc(10px + 2vmin);
+      color: #1a2b42;
+      max-width: 960px;
+      margin: 0 auto;
+      text-align: center;
+      background-color: var(--card-list-background-color);
+    }
+
+    main {
+      flex-grow: 1;
+    }
+
+    .logo {
+      margin-top: 36px;
+      animation: app-logo-spin infinite 20s linear;
+    }
+
+    @keyframes app-logo-spin {
+      from {
+        transform: rotate(0deg);
+      }
+      to {
+        transform: rotate(360deg);
+      }
+    }
+
+    .app-footer {
+      font-size: calc(12px + 0.5vmin);
+      align-items: center;
+    }
+
+    .app-footer a {
+      margin-left: 5px;
+    }
+  `;
+
+  constructor() {
+    super();
+    this.header = 'My app';
+  }
+
+  render() {
+    return html`
+      <main>
+        <coaster-card></coaster-card>
+        <coaster-card></coaster-card>
+        <coaster-card></coaster-card>
+        <coaster-card></coaster-card>
+        <coaster-card></coaster-card>
+        <building-card></building-card>
+        <building-card></building-card>
+        <building-card></building-card>
+        <building-card></building-card>
+        <building-card></building-card>
+      </main>
+
+      <p class="app-footer">
+        🚽 Made with love by
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/open-wc"
+          >open-wc</a
+        >.
+      </p>
+    `;
+  }
+}
+
+customElements.define('card-list', CardList);
